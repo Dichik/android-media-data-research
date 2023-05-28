ae6f602a2565da1571fc48066be86bfb28ae70d8
Dichik <omeluan.dima@gmail.com>
2023-05-28 14:20:50 +0300

    Create Audio and Video activities

---
+++ app/src/main/java/com/example/mediadataresearch/AudioActivity.java
@@ -0,0 +1,14 @@
+package com.example.mediadataresearch;
+
+import android.media.MediaPlayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AudioActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+
+    public AudioActivity() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/mediadataresearch/VideoActivity.java
@@ -0,0 +1,4 @@
+package com.example.mediadataresearch;
+
+public class VideoActivity {
+}
